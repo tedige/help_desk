1e49d16c1af4a9da000e9b0072c58b892eda309d
Tomiris Edige <tomiedige@icloud.com>
2023-10-18 09:39:37 +0600

    fix: allowed all hosts

--- helpdesk_project/helpdesk_project/settings.py
+++ helpdesk_project/helpdesk_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-wqv#_l#3b3!aoki+7hxg%epbij%k26zkxj9_n&#51^coah=_ws
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
